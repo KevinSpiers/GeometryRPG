aaeb0c10b9f85ea20e87df5a604756d64f18012a
KevinSpiers <spiers.24@osu.edu>
2016-11-19 23:57:48 -0500

    lets go

---
+++ Assets/Scripts/Player/Abilities/PlayerAbilities/AbilityObject/SpinObject.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinObject : MonoBehaviour, IAbilityObject {
+
+	private Player player;
+
+	private Vector2 StartLocation;
+	private float MaxDistanceCovered;
+	private int Damage;
+	private Vector2 vectorFromPlayer;
+	private AbilityExpirationTimer timer;
+
+	public void Make(Player _player)
+	{
+		player = _player;
+		StartLocation = player.gameObject.transform.position;
+		MaxDistanceCovered = player.characterClass.ps.AttackRange;
+		Damage = player.characterClass.ps.Attack;
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.transform.position = StartLocation;
+		Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D> ();
+		timer = new AbilityExpirationTimer (2f);
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		//Debug.Log(coll.gameObject.tag);
+		/*if (!coll.gameObject.CompareTag ("Player") && !coll.gameObject.CompareTag("Drop") && !coll.gameObject.CompareTag("Attack")) {
+			if(coll.gameObject.CompareTag("Enemy")){
+				coll.gameObject.GetComponent<IEnemy>().EnemyProp.stats.LoseHealth(Damage);
+			}
+			Destroy(this.gameObject);
+		}*/
+	}
+
+	void Update () 
+	{
+		GetComponent<Rigidbody>().transform.Rotate (Vector3.forward*Time.deltaTime);
+		this.transform.position = player.transform.position;
+		if (timer.shouldDestroy ()) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/Abilities/PlayerAbilities/Spin.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : IAbility {
+
+	private Player player;
+	private CoolDownTimer akd;
+	public CoolDownTimer AbilityCoolDown
+	{ 
+		get
+		{
+			return akd;
+		} 
+	}
+
+	public Spin(Player _player)
+	{
+		player = _player;
+		akd = new CoolDownTimer (3f);
+	}
+
+	private void CreateAttack()
+	{
+		GameObject spin = GameObject.Instantiate(Resources.Load("Prefabs/spin")) as GameObject;
+		SpinObject spinScript = spin.GetComponent<SpinObject>();
+		spinScript.Make(player);
+	}
+
+
+	public void UseAbility()
+	{
+		if (akd.CanUse) {
+			CreateAttack();
+			akd.StartCoolDown();
+		}
+	}
+
+}
