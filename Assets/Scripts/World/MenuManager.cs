3754069f5c4838805f3c85fb8efc46da5a7778ac
cam1000100 <cam1000100@gmail.com>
2016-11-19 19:22:52 -0500

    added more to menumanager

--- Assets/Scripts/World/MenuManager.cs
+++ Assets/Scripts/World/MenuManager.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuManager{
-    int[,] buttonGrid; //value of 1 means there is a button there, 0 means no button
-
+    int[,] buttonExistanceGrid; //value of 1 means there is a button there, 0 means no button
+    Button[,] buttonObjectGrid;
     int selectedRow;
     int selectedColumn;
 
     int rows;
     int columns;
 
-    public MenuManager(int rows, int columns){
-        buttonGrid = new int[rows, columns];
-        selectedColumn = 0;
-        selectedRow = 0;
+    public MenuManager(int rows, int columns, int defaultRow, int defaultCol, Button first){
+        buttonExistanceGrid = new int[rows, columns];
+        selectedColumn = defaultCol;
+        selectedRow = defaultRow;
+
+        SetButton(defaultRow, defaultCol, first);
+
+        //gameObject.GetComponentInChildren<Text>().color = new Color(255, 255, 255, 255);
+
         this.rows = rows;
         this.columns = columns;
         InitializeGrid();
@@ -25,49 +31,68 @@ public class MenuManager{
         {
             for(int j=0; j < columns; j++)
             {
-                buttonGrid[i, j] = 0;
+                buttonExistanceGrid[i, j] = 0;
             }
         }
     }
-    public void SetButton(int row, int column)
+    public void SetButton(int row, int col, Button button)
     {
-        buttonGrid[row, column] = 1;
+        buttonExistanceGrid[row, col] = 1;
+        buttonObjectGrid[row, col] = button;
+    }
+    void Select()
+    {
+        buttonObjectGrid[selectedRow,selectedColumn].gameObject.GetComponentInChildren<Text>().color = new Color(255, 255, 255, 255);
+    }
+    void Deselect()
+    {
+        buttonObjectGrid[selectedRow, selectedColumn].gameObject.GetComponentInChildren<Text>().color = new Color(255, 0, 0, 255);
     }
 
-    public void SelectCurrent()
+    public void ActivateCurrent()
     {
         //find the action associated with the currently selected menu option
+        buttonObjectGrid[selectedRow, selectedColumn].Invoke("Activate", 0);
+
     }
     public void MoveSelectorLeft()
     {
         int newCol = (selectedColumn - 1) % columns;
-        if(buttonGrid[selectedRow, newCol] == 1)
+        if(buttonExistanceGrid[selectedRow, newCol] == 1)
         {
+            Deselect();
             selectedColumn = newCol;
+            Select();
         }
     }
     public void MoveSelectorRight()
     {
         int newCol = (selectedColumn + 1) % columns;
-        if (buttonGrid[selectedRow, newCol] == 1)
+        if (buttonExistanceGrid[selectedRow, newCol] == 1)
         {
+            Deselect();
             selectedColumn = newCol;
+            Select();
         }
     }
     public void MoveSelectorUp()
     {
         int newRow = (selectedRow - 1) % rows;
-        if (buttonGrid[newRow, selectedColumn] == 1)
+        if (buttonExistanceGrid[newRow, selectedColumn] == 1)
         {
+            Deselect();
             selectedRow = newRow;
+            Select();
         }
     }
     public void MoveSelectorDown()
     {
         int newRow = (selectedRow + 1) % rows;
-        if (buttonGrid[newRow, selectedColumn] == 1)
+        if (buttonExistanceGrid[newRow, selectedColumn] == 1)
         {
+            Deselect();
             selectedRow = newRow;
+            Select();
         }
     }
 }
