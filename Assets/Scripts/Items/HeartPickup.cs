f3ba76dd5dd2d67d0f5a0c66b489bad4a3a59ecc
KevinSpiers <spiers.24@osu.edu>
2016-11-20 04:46:21 -0500

    pickups

---
+++ Assets/Scripts/Items/HeartPickup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartPickup : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.GetComponent<Player> () != null) {
+			other.GetComponent<Player> ().characterClass.ps.GainHealth (20);
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Items/ManaPickup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManaPickup : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.GetComponent<Player> () != null) {
+			other.GetComponent<Player> ().characterClass.ps.GainMana (20);
+			Destroy (this.gameObject);
+		}
+	}
+}
